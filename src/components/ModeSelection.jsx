9866d58fbeb80b2d3e4ac6c72f843742b5fb02d1
SalihTpc <salihtopcu@hotmail.com.tr>
2023-12-19 13:34:10 +0300

    bg changed.

--- src/components/ModeSelection.jsx
+++ src/components/ModeSelection.jsx
@@ -4,14 +4,14 @@ const ModeSelection = ({ setMode }) => {
   return (
     <div className="absolute flex items-center flex-col justify-center right-3 gap-2">
       <p
-        className="p-3 text-center text-white cursor-pointer bg-slate-500 w-16 rounded-xl"
+        className="p-3 text-center text-white cursor-pointer bg-red-500 w-16 rounded-xl"
         onClick={() => setMode(false)}
       >
         64
       </p>
       <hr />
       <p
-        className="p-3 text-center text-white cursor-pointer bg-slate-500 w-16 rounded-xl"
+        className="p-3 text-center text-white cursor-pointer bg-red-500 w-16 rounded-xl"
         onClick={() => setMode(true)}
       >
         16X4
